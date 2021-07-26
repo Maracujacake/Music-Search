06f6191d113d3ae1862cb7298235fb4fc17951b1
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-07-26 12:18:39 -0300

    js

---
+++ app.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('#form')
+const searchInput = document.querySelector('#search')
+const songsContainer = document.querySelector('#songs-container')
+const prevAndNextContainer = document.querySelector('#prev-and-next-container')
+
+console.log({form, searchInput, songsContainer, prevAndNextContainer})
+
+const apiUrl = `https://api.lyrics.ovh`
+
+
