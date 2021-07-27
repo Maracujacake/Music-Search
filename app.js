b65509baee13177d017136112bfe502ac87adc34
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-07-27 11:55:09 -0300

    jj

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const searchInput = document.querySelector('#search')
 const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
-console.log({form, searchInput, songsContainer, prevAndNextContainer})
 
 const apiUrl = `https://api.lyrics.ovh`
 
+form.addEventListener('submit', event =>{
+    event.preventDefault()
 
+    const searchTerm = searchInput.value.trim()
+    
+    if(!searchTerm){
+        alertaCampos()
+        songsContainer.innerHTML = `<li class="warning-message">Por favor, preencha os campos</li>`
+        function alertaCampos(){
+            alert("Preencha corretamente os campos")
+        }
+    }
+})
