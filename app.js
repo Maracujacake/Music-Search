f862464576a0ab6e8450fcb49809e356da58ead8
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-07-29 10:51:20 -0300

    joolt

--- app.js
+++ app.js
@@ -6,13 +6,23 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 /* trocar a api pois esta já não está mais funcionando*/ 
 const apiUrl = `https://api.lyrics.ovh`
 const insertSongsIntoPage = songsInfo =>{
-    debugger
     songsContainer.innerHTML = songsContainer.innerHTML = songsInfo.data.map(song => `
     <li class="song">
         <span class="songs-artist"> <strong> ${song.artist.name} </strong> - ${song.title} </span>
         <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}"> Ver Letra </button>
     </li>`).join('') 
+
+    if (songsInfo.prev || songsInfo.next){
+        prevAndNextContainer.innerHTML = `
+            ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ` ` }
+            ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ` ` }
+        `
+    }
+
+    prevAndNextContainer.innerHTML
 }
+
+
 
 const fetchSongs = async term => {
     const response = await fetch(`${apiUrl}/suggest/${term}`)
